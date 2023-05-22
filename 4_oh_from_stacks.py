5e508f2217e7348f0f662850f2b0c10085b64291
Shawn O <szo0032@auburn.edu>
2023-05-22 12:44:13 -0500

    update oh_from_stacks for multiple rednesses

--- 4_oh_from_stacks.py
+++ 4_oh_from_stacks.py
@@ -7,6 +7,7 @@ import numpy as np
 import logging as log
 
 from astropy.time import Time
+from astropy.io import fits
 from argparse import ArgumentParser
 import matplotlib.pyplot as plt
 from astropy.visualization import (
@@ -15,13 +16,13 @@ from astropy.visualization import (
 from typing import Tuple
 
 from read_swift_config import read_swift_config
-from swift_types import SwiftFilter, SwiftStackingMethod, SwiftUVOTImage
+from swift_types import SwiftFilter, StackingMethod, SwiftUVOTImage
 from reddening_correction import DustReddeningPercent
 from stack_info import stacked_images_from_stackinfo
 from fluorescence_OH import flux_OH_to_num_OH, read_gfactor_1au
 from flux_OH import OH_flux_from_count_rate, OH_flux_from_count_rate_fixed_beta
 from aperture_photometry import do_aperture_photometry
-from vectorial_model import num_OH_to_Q
+from num_OH_to_Q import num_OH_to_Q_vectorial
 
 __version__ = "0.0.1"
 
@@ -69,7 +70,8 @@ def show_fits_scaled(image_sum, image_median):
     vmin, vmax = zscale.get_limits(image_sum)
 
     im1 = ax1.imshow(image_sum, vmin=vmin, vmax=vmax)
-    im2 = ax2.imshow(image_median, vmin=vmin, vmax=vmax)
+    # im2 = ax2.imshow(image_median, vmin=vmin, vmax=vmax)
+    ax2.imshow(image_median, vmin=vmin, vmax=vmax)
     fig.colorbar(im1)
 
     plt.show()
@@ -120,8 +122,8 @@ def pad_to_match_sizes(
 
 
 def show_fits_subtracted(uw1_stack, uvv_stack, beta):
-    uw1 = uw1_stack.stacked_image
-    uvv = uvv_stack.stacked_image
+    uw1 = uw1_stack.stacked_image / uw1_stack.exposure_time
+    uvv = uvv_stack.stacked_image / uvv_stack.exposure_time
 
     uw1, uvv = pad_to_match_sizes(uw1, uvv)
     dust_subtracted = uw1 - beta * uvv
@@ -136,6 +138,8 @@ def show_fits_subtracted(uw1_stack, uvv_stack, beta):
     # im2 = ax2.imshow(image_median, vmin=vmin, vmax=vmax)
     fig.colorbar(im1)
 
+    hdu = fits.PrimaryHDU(dust_subtracted)
+    hdu.writeto("subtracted.fits", overwrite=True)
     plt.show()
 
 
@@ -159,83 +163,94 @@ def main():
     # collect aperture results for uw1 and uvv filters from the stacked images
     aperture_results = {}
     for filter_type in [SwiftFilter.uvv, SwiftFilter.uw1]:
-        summed_image = stacked_images[(filter_type, SwiftStackingMethod.summation)]
-        median_image = stacked_images[(filter_type, SwiftStackingMethod.median)]
+        summed_image = stacked_images[(filter_type, StackingMethod.summation)]
+        median_image = stacked_images[(filter_type, StackingMethod.median)]
         # show_fits_scaled(summed_image.stacked_image, median_image.stacked_image)
         aperture_results[filter_type] = do_aperture_photometry(
             summed_image, median_image
         )
 
-    dust_redness = DustReddeningPercent(10)
-    # calculate OH flux based on the aperture results in uw1 and uvv filters
-    print(f"\n\nCalculating OH flux with dust redness {dust_redness.reddening}% ...")
-    flux_OH_1 = OH_flux_from_count_rate(
-        solar_spectrum_path=swift_config["solar_spectrum_path"],
-        # solar_spectrum_time=Time("2457422", format="jd"),
-        # solar_spectrum_time=Time("2016-02-01"),
-        solar_spectrum_time=Time(
-            stacked_images[
-                (SwiftFilter.uw1, SwiftStackingMethod.summation)
-            ].observation_mid_time,
-            format="fits",
-        ),
-        effective_area_uw1_path=swift_config["effective_area_uw1_path"],
-        effective_area_uvv_path=swift_config["effective_area_uvv_path"],
-        result_uw1=aperture_results[SwiftFilter.uw1],
-        result_uvv=aperture_results[SwiftFilter.uvv],
-        dust_redness=dust_redness,
+    # dust_redness = DustReddeningPercent(10)
+    dust_redness_list = list(
+        map(lambda x: DustReddeningPercent(x), [0, 5, 10, 15, 20, 25])
     )
-    flux_OH_2 = OH_flux_from_count_rate_fixed_beta(
-        effective_area_uw1_path=swift_config["effective_area_uw1_path"],
-        effective_area_uvv_path=swift_config["effective_area_uvv_path"],
-        result_uw1=aperture_results[SwiftFilter.uw1],
-        result_uvv=aperture_results[SwiftFilter.uvv],
-        dust_redness=dust_redness,
-    )
+    for dust_redness in dust_redness_list:
+        # calculate OH flux based on the aperture results in uw1 and uvv filters
+        print(
+            f"\n\nCalculating OH flux with dust redness {dust_redness.reddening}% ..."
+        )
+        flux_OH_1 = OH_flux_from_count_rate(
+            solar_spectrum_path=swift_config["solar_spectrum_path"],
+            # solar_spectrum_time=Time("2457422", format="jd"),
+            # solar_spectrum_time=Time("2016-02-01"),
+            solar_spectrum_time=Time(
+                stacked_images[
+                    (SwiftFilter.uw1, StackingMethod.summation)
+                ].observation_mid_time,
+                format="fits",
+            ),
+            effective_area_uw1_path=swift_config["effective_area_uw1_path"],
+            effective_area_uvv_path=swift_config["effective_area_uvv_path"],
+            result_uw1=aperture_results[SwiftFilter.uw1],
+            result_uvv=aperture_results[SwiftFilter.uvv],
+            dust_redness=dust_redness,
+        )
+        flux_OH_2 = OH_flux_from_count_rate_fixed_beta(
+            effective_area_uw1_path=swift_config["effective_area_uw1_path"],
+            effective_area_uvv_path=swift_config["effective_area_uvv_path"],
+            result_uw1=aperture_results[SwiftFilter.uw1],
+            result_uvv=aperture_results[SwiftFilter.uvv],
+            dust_redness=dust_redness,
+        )
 
-    fluorescence_data = read_gfactor_1au(swift_config["oh_fluorescence_path"])
-    img = stacked_images[(SwiftFilter.uw1, SwiftStackingMethod.summation)]
-    num_OH_1 = flux_OH_to_num_OH(
-        flux_OH=flux_OH_1,
-        helio_r_au=img.helio_r_au,
-        helio_v_kms=img.helio_v_kms,
-        delta_au=img.delta_au,
-        fluorescence_data=fluorescence_data,
-    )
-    num_OH_2 = flux_OH_to_num_OH(
-        flux_OH=flux_OH_2,
-        helio_r_au=img.helio_r_au,
-        helio_v_kms=img.helio_v_kms,
-        delta_au=img.delta_au,
-        fluorescence_data=fluorescence_data,
-    )
-    print("")
-    print(f"Total number of OH, beta from solar spectrum method: {num_OH_1}")
-    print(f"Total number of OH, fixed beta method: {num_OH_2}")
+        fluorescence_data = read_gfactor_1au(swift_config["oh_fluorescence_path"])
+        img = stacked_images[(SwiftFilter.uw1, StackingMethod.summation)]
+        num_OH_1 = flux_OH_to_num_OH(
+            flux_OH=flux_OH_1,
+            helio_r_au=img.helio_r_au,
+            helio_v_kms=img.helio_v_kms,
+            delta_au=img.delta_au,
+            fluorescence_data=fluorescence_data,
+        )
+        num_OH_2 = flux_OH_to_num_OH(
+            flux_OH=flux_OH_2,
+            helio_r_au=img.helio_r_au,
+            helio_v_kms=img.helio_v_kms,
+            delta_au=img.delta_au,
+            fluorescence_data=fluorescence_data,
+        )
+        print("")
+        print(f"Total number of OH, beta from solar spectrum method: {num_OH_1}")
+        print(f"Total number of OH, fixed beta method: {num_OH_2}")
 
-    Q1 = num_OH_to_Q(
-        helio_r=img.helio_r_au,
-        num_OH=num_OH_1,
-        vectorial_model_path=swift_config["vectorial_model_path"],
-    )
-    Q2 = num_OH_to_Q(
-        helio_r=img.helio_r_au,
-        num_OH=num_OH_2,
-        vectorial_model_path=swift_config["vectorial_model_path"],
-    )
-    print("")
-    print(f"Heliocentric comet distance: {img.helio_r_au}")
-    print(f"Q(H2O) from N(OH), first method: {Q1} mol/s at {img.observation_mid_time}")
-    print(f"Q(H2O) from N(OH), second method: {Q2} mol/s at {img.observation_mid_time}")
+        Q1 = num_OH_to_Q_vectorial(
+            helio_r=img.helio_r_au,
+            num_OH=num_OH_1,
+            vectorial_model_path=swift_config["vectorial_model_path"],
+        )
+        Q2 = num_OH_to_Q_vectorial(
+            helio_r=img.helio_r_au,
+            num_OH=num_OH_2,
+            vectorial_model_path=swift_config["vectorial_model_path"],
+        )
+        print("")
+        print(f"Heliocentric comet distance: {img.helio_r_au}")
+        print(
+            f"Q(H2O) from N(OH), first method: {Q1} mol/s at {img.observation_mid_time}"
+        )
+        print(
+            f"Q(H2O) from N(OH), second method: {Q2} mol/s at {img.observation_mid_time}"
+        )
+
+    # show_fits_subtracted(
+    #     stacked_images[(SwiftFilter.uw1, SwiftStackingMethod.summation)],
+    #     stacked_images[(SwiftFilter.uvv, SwiftStackingMethod.summation)],
+    #     beta=0.101483,
+    # )
 
     show_fits_subtracted(
-        stacked_images[(SwiftFilter.uw1, SwiftStackingMethod.summation)],
-        stacked_images[(SwiftFilter.uvv, SwiftStackingMethod.summation)],
-        beta=0.101483,
-    )
-    show_fits_subtracted(
-        stacked_images[(SwiftFilter.uw1, SwiftStackingMethod.median)],
-        stacked_images[(SwiftFilter.uvv, SwiftStackingMethod.median)],
+        stacked_images[(SwiftFilter.uw1, StackingMethod.median)],
+        stacked_images[(SwiftFilter.uvv, StackingMethod.median)],
         beta=0.101483,
     )
 
