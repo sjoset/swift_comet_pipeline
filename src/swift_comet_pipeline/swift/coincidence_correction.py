1dc987e46bea08dbe2c4e586d43a3232209baab3
Shawn O <szo0032@auburn.edu>
2024-09-26 20:57:51 -0500

    Move coincidence correction to kernel-based approach and add scikit-image as dependency

--- src/swift_comet_pipeline/swift/coincidence_correction.py
+++ src/swift_comet_pipeline/swift/coincidence_correction.py
@@ -1,12 +1,19 @@
 import copy
 import math
+
 import numpy as np
+from scipy.signal import convolve2d
 
 from swift_comet_pipeline.swift.uvot_image import SwiftUVOTImage, SwiftPixelResolution
 
 
 class CoincidenceCorrection:
+
+    kernel = {}
+
     def __init__(self) -> None:
+        # TODO: makes these class variables also, or rewrite this without using a class
+
         # Poole 2008
         # polynomial coefficients
         self.a1: float = -0.0663428
@@ -20,6 +27,13 @@ class CoincidenceCorrection:
         self.frame_time: float = 0.0110329
 
         self.poly = np.poly1d([self.a4, self.a3, self.a2, self.a1, 1])
+
+        if not CoincidenceCorrection.kernel:
+            for scale in [
+                SwiftPixelResolution.data_mode,
+                SwiftPixelResolution.event_mode,
+            ]:
+                self.kernel[scale] = self.make_kernel(image_scale=scale)
 
     def coi_factor(self, raw_pixel_count_rate: float) -> float:
         x = self.alpha * raw_pixel_count_rate * self.frame_time
@@ -35,15 +49,46 @@ class CoincidenceCorrection:
 
         return ratio
 
+    def make_kernel(self, image_scale: SwiftPixelResolution) -> np.ndarray:
+        """
+        Make a gaussian psf, returned as a 2d array, with the gaussian peak in the middle of the array in both dimensions
+        """
+        kernel_radius = np.ceil(5.0 / float(image_scale)).astype(np.int32)
+        # the edge of our psf will encapsulate up to 3 sigma of the psf
+        sigma = kernel_radius / 3.0
+        x = np.arange(-kernel_radius // 2 + 1, kernel_radius // 2 + 1)
+        y = np.arange(-kernel_radius // 2 + 1, kernel_radius // 2 + 1)
+        x, y = np.meshgrid(x, y)
+        psf = np.exp(-(x**2 + y**2) / (2.0 * sigma**2))
+        # normalize the psf
+        psf /= psf.sum()
+        return psf
 
-# TODO: https://docs.astropy.org/en/stable/convolution/index.html
 
+def coincidence_correction(
+    img: SwiftUVOTImage, scale: SwiftPixelResolution
+) -> np.ndarray:
 
-def coincidence_correction(img: SwiftUVOTImage, scale: SwiftPixelResolution):
-    # TODO: document and cite Poole 2008
+    # make a copy so we can apply coincidence correction without altering original
     img_data = copy.deepcopy(img)
+    # Replace the padding pixels (zeros) with a very small value to avoid division by zero
+    dead_space_mask = img_data == 0
+    img_data[dead_space_mask] = 1e-29
 
-    # TODO: reimplement this in terms of a convolution and kernel
+    coi = CoincidenceCorrection()
+    kernel = coi.kernel[scale]
+
+    with np.errstate(divide="ignore", invalid="ignore"):
+        coi_map: SwiftUVOTImage = coi.coi_factor(convolve2d(img_data, kernel, mode="same"))  # type: ignore
+
+    # Replace the zero pixels with ones: we multiply the image by coi_map, so this means no correction
+    zeros_mask = coi_map == 0.0
+    coi_map[zeros_mask] = 1.0
+    return coi_map
+
+
+def coincidence_correction_old(img: SwiftUVOTImage, scale: SwiftPixelResolution):
+    img_data = copy.deepcopy(img)
 
     # the padding around the images from swift are pure zeros - we have to divide by the pixel value
     # in CoincidenceCorrection.coi_factor() so change the padding pixels to be slightly non-zero
