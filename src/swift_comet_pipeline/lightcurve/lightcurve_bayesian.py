Fix probability not being re-normalized among valid production rates in bayesian lightcurve calculation

src/swift_comet_pipeline/lightcurve/lightcurve_bayesian.py
@@ -104,9 +104,14 @@ def bayesian_lightcurve_from_aperture_lightcurve(
     non_detection_probs = []
     for _, sub_df in filled_in_df.groupby("time_from_perihelion_days"):
         non_zero_prod_mask = sub_df.q != 0.0
+
+        # calculate the detection probability and divide by it to re-normalize the probability
+        # among valid production rates
+        detection_prob = np.sum(sub_df.redness_prior_prob[non_zero_prod_mask])
         # Expectation value of q, with sum taken over the redness
-        posterior_q = np.sum(sub_df.posterior_qs[non_zero_prod_mask])
+        posterior_q = np.sum(sub_df.posterior_qs[non_zero_prod_mask]) / detection_prob
         posterior_q_list.append(posterior_q)
+
         non_detection_prob = np.sum(sub_df.redness_prior_prob[~non_zero_prod_mask])
         non_detection_probs.append(non_detection_prob)
 

src/swift_comet_pipeline/spectrum/solar_spectrum.py
@@ -123,7 +123,7 @@ def solar_count_rate_in_filter(
         np.c_[lambdas, solar_irradiances_on_filter_lambdas.T], ea_responses_on_lambdas.T
     ]
 
-    # TODO: magic numbers
+    # TODO: magic numbers and rewrite this function - ask Lucy about these
     cr = (
         np.sum(spec[:, 0] * spec[:, 1] * spec[:, 2]) * dlambda * 1e7 * 5.034116651114543
     )