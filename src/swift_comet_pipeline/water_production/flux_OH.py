c47e1d52c50867d7ffe6ef8f214c5712865d0ae7
Shawn O <szo0032@auburn.edu>
2025-06-16 11:19:24 -0500

    Add small TODO for flux

--- src/swift_comet_pipeline/water_production/flux_OH.py
+++ src/swift_comet_pipeline/water_production/flux_OH.py
@@ -15,6 +15,8 @@ def OH_flux_from_count_rate(
     uvv: CountRate,
     beta: DustReddeningPercent,
 ) -> OHFlux:
+    # TODO: document the units of alpha here
+
     # this comes from an OH spectral model in Bodewits et. al 2019 by convolving the OH spectrum through the uw1 filter
     # to convert count rate to flux
     alpha = 1.2750906353215913e-12
