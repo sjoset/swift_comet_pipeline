d70346168298e03a5a4e9e0219417564edf8b6f6
Shawn O <szo0032@auburn.edu>
2025-07-07 14:06:43 -0500

    Update dust reddening code to include upper and lower limits based on effective filter wavelengths

---
+++ src/swift_comet_pipeline/dust/dust_limits.py
@@ -0,0 +1,47 @@
+from functools import cache
+
+import astropy.units as u
+
+from swift_comet_pipeline.swift.effective_wavelength import (
+    effective_wavelength_of_filter,
+)
+from swift_comet_pipeline.types.dust_reddening_percent import DustReddeningPercent
+from swift_comet_pipeline.types.swift_filter import SwiftFilter
+
+"""
+Our filters are separated by 2770 angstroms, so our % per 1000 angstroms needs to be capped to avoid negative count rates for either filter
+as a result of our linear approximation extending too far
+
+TODO: show derivation or cite after publishing
+"""
+
+
+@cache
+def get_dust_redness_lower_limit() -> DustReddeningPercent:
+    """
+    Returns the minimum dust redness that the uvw1 and uvv filter pair can measure with a linear approximation for the spectral slope
+    """
+
+    l_uvw1 = effective_wavelength_of_filter(SwiftFilter.uw1)
+    l_uvv = effective_wavelength_of_filter(SwiftFilter.uvv)
+    dlambda = l_uvv - l_uvw1
+
+    min_redness = -20000.0 / dlambda.to_value(u.nm)  # type: ignore
+
+    return DustReddeningPercent(min_redness)
+
+
+@cache
+def get_dust_redness_upper_limit() -> DustReddeningPercent:
+    """
+    Returns the maximum dust redness that the uvw1 and uvv filter pair can measure with a linear approximation for the spectral slope
+    Redness is in percent per 100 nm or 1000 angstroms
+    """
+
+    l_uvw1 = effective_wavelength_of_filter(SwiftFilter.uw1)
+    l_uvv = effective_wavelength_of_filter(SwiftFilter.uvv)
+    dlambda = l_uvv - l_uvw1
+
+    max_redness = 20000 / dlambda.to_value(u.nm)  # type: ignore
+
+    return DustReddeningPercent(max_redness)
--- src/swift_comet_pipeline/dust/reddening_correction.py
+++ src/swift_comet_pipeline/dust/reddening_correction.py
@@ -1,53 +1,19 @@
 from functools import cache
 
-import numpy as np
+# import numpy as np
+import astropy.units as u
 
-from swift_comet_pipeline.swift.read_filter_effective_area import (
-    read_filter_effective_area,
+from swift_comet_pipeline.swift.effective_wavelength import (
+    effective_wavelength_of_filter,
 )
+
+# from swift_comet_pipeline.swift.read_filter_effective_area import (
+#     read_filter_effective_area,
+# )
 from swift_comet_pipeline.types import DustReddeningPercent, SwiftFilter
 
 
 __all__ = ["reddening_correction"]
-
-
-@cache
-def _pre_middle_factor() -> float:
-
-    ea_data_uw1 = read_filter_effective_area(filter_type=SwiftFilter.uw1)
-    ea_data_uvv = read_filter_effective_area(filter_type=SwiftFilter.uvv)
-    if ea_data_uw1 is None or ea_data_uvv is None:
-        return np.nan
-
-    uw1_lambdas = ea_data_uw1.lambdas_nm
-    uw1_responses = ea_data_uw1.responses_cm2
-
-    uvv_lambdas = ea_data_uvv.lambdas_nm
-    uvv_responses = ea_data_uvv.responses_cm2
-
-    wave_uw1 = 0
-    ea_uw1 = 0
-    wave_v = 0
-    ea_v = 0
-
-    # TODO: rewrite this without loops
-    delta_wave_uw1 = uw1_lambdas[1] - uw1_lambdas[0]
-    delta_wave_v = uvv_lambdas[1] - uvv_lambdas[0]
-    for i in range(len(uw1_lambdas)):
-        wave_uw1 += uw1_lambdas[i] * uw1_responses[i] * delta_wave_uw1
-        ea_uw1 += uw1_responses[i] * delta_wave_uw1
-    wave_uw1 = wave_uw1 / ea_uw1
-    for i in range(len(uvv_lambdas)):
-        wave_v += uvv_lambdas[i] * uvv_responses[i] * delta_wave_v
-        ea_v += uvv_responses[i] * delta_wave_v
-    wave_v = wave_v / ea_v
-
-    # TODO: magic number
-    # TODO: get reddening correction factor: do this with proper units (when EA lambdas are in angstroms, this is 200000)
-    pre_middle_factor = (wave_v - wave_uw1) / 20000
-
-    # multiply by dust_redness in percent per 100 nm to get the 'middle factor' in reddening_correction()
-    return pre_middle_factor
 
 
 @cache
@@ -57,9 +23,14 @@ def reddening_correction(dust_redness: DustReddeningPercent) -> float:
     units of reddening: %/100nm
 
     where beta is the factor in (uw1 - beta * uvv)
+
+    TODO: document derivation in thesis, including explanation of magic number 20000 % per nm
     """
 
-    middle_factor = _pre_middle_factor() * dust_redness
+    l_uvw1 = effective_wavelength_of_filter(SwiftFilter.uw1)
+    l_uvv = effective_wavelength_of_filter(SwiftFilter.uvv)
 
-    # Reference: eq. 3.36 and 3.39 in Xing thesis
-    return (1 - middle_factor) / (1 + middle_factor)
+    dlambda_nm = (l_uvv - l_uvw1).to_value(u.nm)  # type: ignore
+    t = dust_redness * dlambda_nm / 20000
+
+    return (1 - t) / (1 + t)
