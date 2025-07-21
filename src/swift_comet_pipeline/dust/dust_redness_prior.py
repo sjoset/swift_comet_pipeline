cd0c1c4a22db63d15ccf52d58e7d196e200ce8b8
Shawn O <szo0032@auburn.edu>
2025-07-21 11:29:01 -0500

    Update dust redness prior and citations

--- src/swift_comet_pipeline/dust/dust_redness_prior.py
+++ src/swift_comet_pipeline/dust/dust_redness_prior.py
@@ -6,6 +6,12 @@ from swift_comet_pipeline.types import DustReddeningPercent
 
 # Assume a gaussian distribution of comet dust redness, with mean and standard deviation below
 def get_dust_redness_mean_prior() -> DustReddeningPercent:
+    """
+    TODO: fill in DOI below
+    Alvarez-Candal 2025, "X-SHOOTER Spectrum of Comet C/2025 N1: Insights into a Distant Interstellar Visitor", doi:
+
+    TODO: place citations
+    """
     return DustReddeningPercent(35.0)
 
 
