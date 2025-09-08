4640e288a3cd372172ed95b4073cf913b2b36140
Shawn O <szo0032@auburn.edu>
2025-09-08 10:51:22 -0500

    Update aperture photometry for count rate, shot noise, and uncertainty in median

--- src/swift_comet_pipeline/aperture/aperture_count_rate.py
+++ src/swift_comet_pipeline/aperture/aperture_count_rate.py
@@ -1,5 +1,8 @@
+from dataclasses import dataclass
+
 import numpy as np
 from photutils.aperture import Aperture, ApertureStats
+from photutils.aperture.stats import SigmaClip
 
 from swift_comet_pipeline.types.background_result import (
     BackgroundResult,
@@ -9,104 +12,206 @@ from swift_comet_pipeline.types.count_rate import CountRate
 from swift_comet_pipeline.types.swift_uvot_image import SwiftUVOTImage
 
 
-# TODO:
-# We need three functions: total, mean, and median count rate
+# TODO: move this to types
+@dataclass
+class ApertureCountRateAnalysis:
+    """
+    Given an aperture, we want these results for determining the signal within it
+    """
+
+    total_count_rate: float
+    median_count_rate: float
+    mean_count_rate: float
+    # calculated with the sum of the count rates over the exposure time
+    count_rate_shot_noise_variance: float
+    # what variance the background contributed, total
+    bg_variance: float
+    # total variance of the sum: count_rate_shot_noise_variance + bg_variance
+    total_count_rate_variance: float
+    # variance if we use the median
+
+    # if any sigma clipping was used
+    sigma_clip: float
+    # total valid pixels left in aperture for signal - sigma clipping may remove some
+    ap_num_pixels: float
 
 
-# def total_count_rate_at_circular_aperture(
+def aperture_analysis(
+    img: SwiftUVOTImage,
+    ap: Aperture,
+    background: BackgroundResult | None,
+    exposure_time_s: float,
+    sigma_clip: float = 3.0,
+) -> ApertureCountRateAnalysis:
+    """
+    Takes an aperture and returns ApertureCountRateAnalysis
+    If a background has not been subtracted, set background to None
+    Otherwise, assumes the background has been subtracted, and uses 'background' to factor in bg error
+    """
+
+    ap_stats = ApertureStats(
+        img, ap, sigma_clip=SigmaClip(sigma=sigma_clip, cenfunc="median")
+    )
+
+    # count rate measures
+    total_count_rate: float = ap_stats.sum  # type: ignore
+    median_count_rate: float = ap_stats.median  # type: ignore
+    mean_count_rate: float = ap_stats.mean  # type: ignore
+    ap_num_pixels: float = ap_stats.sum_aper_area.value
+
+    # variances
+    count_rate_shot_noise_variance: float = ap_stats.sum / exposure_time_s  # type: ignore
+    # net negative count rate - estimate the error
+    if count_rate_shot_noise_variance < 0.0:
+        count_rate_shot_noise_variance = np.abs(count_rate_shot_noise_variance)
+
+    if background is None:
+        bg_variance = 0.0
+    else:
+        k = 1 if background.bg_estimator == BackgroundValueEstimator.mean else np.pi / 2
+        bg_shot_noise_variance = background.bg_shot_noise_variance
+        bg_area = background.bg_num_pixels
+
+        bg_variance = (
+            ap_num_pixels * bg_shot_noise_variance * (1 + (k * ap_num_pixels) / bg_area)
+        )
+
+    total_variance = count_rate_shot_noise_variance + bg_variance
+
+    return ApertureCountRateAnalysis(
+        total_count_rate=total_count_rate,
+        median_count_rate=median_count_rate,
+        mean_count_rate=mean_count_rate,
+        count_rate_shot_noise_variance=count_rate_shot_noise_variance,
+        bg_variance=bg_variance,
+        total_count_rate_variance=total_variance,
+        ap_num_pixels=ap_num_pixels,
+        sigma_clip=sigma_clip,
+    )
+
+
+# def total_aperture_count_rate(
 #     img: SwiftUVOTImage,
-#     aperture_center: PixelCoord,
-#     aperture_radius: float,
+#     ap: Aperture,
 #     background: BackgroundResult | None,
 #     exposure_time_s: float,
 # ) -> CountRate:
+#     """
+#     Takes the sum of pixels inside as the count rate, along with its error
+#     If a background has not been subtracted, set background to None
+#     Otherwise, assumes the background has been subtracted from 'img'
+#     """
 #
-#     ap = CircularAperture((aperture_center.x, aperture_center.y), r=aperture_radius)
-#     return total_aperture_count_rate(
-#         img=img, ap=ap, background=background, exposure_time_s=exposure_time_s
-#     )
+#     ap_stats = ApertureStats(img, ap)
+#
+#     total_ap_count_rate = float(ap_stats.sum)
+#     aperture_area_pix = ap_stats.sum_aper_area.value
+#
+#     source_variance = total_ap_count_rate / exposure_time_s
+#
+#     # net negative count rate - estimate the error
+#     if source_variance < 0.0:
+#         source_variance = np.abs(source_variance)
+#
+#     if background is not None:
+#         k = 1 if background.bg_estimator == BackgroundValueEstimator.mean else np.pi / 2
+#         bg_variance_per_pixel = background.count_rate_per_pixel.sigma**2
+#         bg_area = background.bg_aperture_area
+#
+#         bg_variance = (
+#             aperture_area_pix
+#             * bg_variance_per_pixel
+#             * (1 + (k * aperture_area_pix) / (exposure_time_s * bg_area))
+#         )
+#     else:
+#         bg_variance = 0.0
+#
+#     total_variance = source_variance + bg_variance
+#
+#     return CountRate(total_ap_count_rate, sigma=np.sqrt(total_variance))
 
 
-def total_aperture_count_rate(
-    img: SwiftUVOTImage,
-    ap: Aperture,
-    background: BackgroundResult | None,
-    exposure_time_s: float,
-) -> CountRate:
-    """
-    Takes the sum of pixels inside as the count rate, along with its error
-    If a background has not been subtracted, set background to None
-    Otherwise, assumes the background has been subtracted from 'img'
-    """
+# def median_aperture_count_rate(
+#     img: SwiftUVOTImage,
+#     ap: Aperture,
+#     background: BackgroundResult | None,
+#     exposure_time_s: float,
+# ) -> CountRate:
+#     """
+#     Takes an aperture and takes the sum of pixels inside as the count rate, along with its error
+#     If a background has not been subtracted, set background to None
+#     Otherwise, assumes the background has been subtracted
+#     """
+#
+#     ap_stats = ApertureStats(img, ap)
+#
+#     aperture_area_pix = ap_stats.sum_aper_area.value
+#
+#     # estimate variance from MAD - median absolute deviation
+#     mad = float(ap_stats.mad_std)
+#
+#     source_variance = (np.pi / 2) * mad**2 / (aperture_area_pix * exposure_time_s)
+#
+#     # net negative count rate - estimate the error
+#     if source_variance < 0.0:
+#         source_variance = np.abs(source_variance)
+#
+#     if background is not None:
+#         k = 1 if background.bg_estimator == BackgroundValueEstimator.mean else np.pi / 2
+#         bg_variance_per_pixel = background.count_rate_per_pixel.sigma**2
+#         bg_area = background.bg_aperture_area
+#
+#         bg_variance = (
+#             aperture_area_pix
+#             * bg_variance_per_pixel
+#             * (1 + (k * aperture_area_pix) / (exposure_time_s * bg_area))
+#         )
+#     else:
+#         bg_variance = 0.0
+#
+#     total_variance = source_variance + bg_variance
+#
+#     return CountRate(float(ap_stats.median), sigma=np.sqrt(total_variance))
 
-    ap_stats = ApertureStats(img, ap)
 
-    total_ap_count_rate = float(ap_stats.sum)
-    aperture_area_pix = ap_stats.sum_aper_area.value
-
-    source_variance = total_ap_count_rate / exposure_time_s
-
-    # net negative count rate - estimate the error
-    if source_variance < 0.0:
-        source_variance = np.abs(source_variance)
-
-    if background is not None:
-        k = 1 if background.bg_estimator == BackgroundValueEstimator.mean else np.pi / 2
-        bg_variance_per_pixel = background.count_rate_per_pixel.sigma**2
-        bg_area = background.bg_aperture_area
-
-        bg_variance = (
-            aperture_area_pix
-            * bg_variance_per_pixel
-            * (1 + (k * aperture_area_pix) / (exposure_time_s * bg_area))
-        )
-    else:
-        bg_variance = 0.0
-
-    total_variance = source_variance + bg_variance
-
-    return CountRate(total_ap_count_rate, sigma=np.sqrt(total_variance))
-
-
-def median_aperture_count_rate(
-    img: SwiftUVOTImage,
-    ap: Aperture,
-    background: BackgroundResult | None,
-    exposure_time_s: float,
-) -> CountRate:
-    """
-    Constructs a circular aperture of aperture_radius at aperture_center and takes the sum of pixels
-    inside as the count rate, along with its error
-    If a background has not been subtracted, set background to None
-    Otherwise, assumes the background has been subtracted
-    """
-
-    ap_stats = ApertureStats(img, ap)
-
-    aperture_area_pix = ap_stats.sum_aper_area.value
-
-    # estimate variance from MAD - median absolute deviation
-    mad = float(ap_stats.mad_std)
-
-    source_variance = (np.pi / 2) * mad**2 / (aperture_area_pix * exposure_time_s)
-
-    # net negative count rate - estimate the error
-    if source_variance < 0.0:
-        source_variance = np.abs(source_variance)
-
-    if background is not None:
-        k = 1 if background.bg_estimator == BackgroundValueEstimator.mean else np.pi / 2
-        bg_variance_per_pixel = background.count_rate_per_pixel.sigma**2
-        bg_area = background.bg_aperture_area
-
-        bg_variance = (
-            aperture_area_pix
-            * bg_variance_per_pixel
-            * (1 + (k * aperture_area_pix) / (exposure_time_s * bg_area))
-        )
-    else:
-        bg_variance = 0.0
-
-    total_variance = source_variance + bg_variance
-
-    return CountRate(float(ap_stats.median), sigma=np.sqrt(total_variance))
+# def median_aperture_count_rate(
+#     img: SwiftUVOTImage,
+#     ap: Aperture,
+#     background: BackgroundResult | None,
+#     exposure_time_s: float,
+# ) -> CountRate:
+#     """
+#     Takes an aperture and takes the sum of pixels inside as the count rate, along with its error
+#     If a background has not been subtracted, set background to None
+#     Otherwise, assumes the background has been subtracted, and uses 'background' to factor in bg error
+#     """
+#
+#     ap_stats = ApertureStats(img, ap)
+#     # ap_stats = ApertureStats(
+#     #     img, ap, sigma_clip=SigmaClip(sigma=sigma_clip, cenfunc="median")
+#     # )
+#
+#     aperture_area_pix = ap_stats.sum_aper_area.value
+#
+#     source_variance = ap_stats.sum / exposure_time_s
+#
+#     # net negative count rate - estimate the error
+#     if source_variance < 0.0:
+#         source_variance = np.abs(source_variance)
+#
+#     if background is None:
+#         bg_variance = 0.0
+#     else:
+#         k = 1 if background.bg_estimator == BackgroundValueEstimator.mean else np.pi / 2
+#         bg_shot_noise_variance = background.bg_shot_noise_variance
+#         bg_area = background.bg_num_pixels
+#
+#         bg_variance = (
+#             aperture_area_pix
+#             * bg_shot_noise_variance
+#             * (1 + (k * aperture_area_pix) / bg_area)
+#         )
+#
+#     total_variance = source_variance + bg_variance
+#
+#     return CountRate(float(ap_stats.median), sigma=np.sqrt(total_variance))
--- src/swift_comet_pipeline/aperture/q_vs_aperture_radius.py
+++ src/swift_comet_pipeline/aperture/q_vs_aperture_radius.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 from rich import print as rprint
 import astropy.units as u
 
-from swift_comet_pipeline.aperture.aperture_count_rate import median_aperture_count_rate
+from swift_comet_pipeline.aperture.aperture_count_rate import aperture_analysis
 from swift_comet_pipeline.aperture.concentric_annuli import (
     make_concentric_annular_apertures,
 )
@@ -88,10 +88,8 @@ def aperture_count_rate_analysis(
         dust_redness=float(dust_redness),
         counts_uw1=uw1_count_rate.value,
         counts_uw1_err=uw1_count_rate.sigma,
-        snr_uw1=uw1_count_rate.value / uw1_count_rate.sigma,
         counts_uvv=uvv_count_rate.value,
         counts_uvv_err=uvv_count_rate.sigma,
-        snr_uvv=uvv_count_rate.value / uvv_count_rate.sigma,
         magnitude_uw1=uw1_magnitude.value,
         magnitude_uw1_err=uw1_magnitude.sigma,
         magnitude_uvv=uvv_magnitude.value,
@@ -145,8 +143,8 @@ def q_vs_aperture_radius(
     drs = np.array([uw1_apertures[0].r]) + np.diff(aperture_radii)  # type: ignore
 
     print("Starting UVW1 counts ...")
-    uw1_annular_median_count_rates = [
-        median_aperture_count_rate(
+    uw1_annular_analyses = [
+        aperture_analysis(
             img=uw1_img,
             ap=ap,
             background=uw1_bg,
@@ -156,7 +154,7 @@ def q_vs_aperture_radius(
     ]
     print("Starting UVV counts ...")
     uvv_annular_median_count_rates = [
-        median_aperture_count_rate(
+        aperture_analysis(
             img=uvv_img,
             ap=ap,
             background=uvv_bg,
@@ -167,14 +165,15 @@ def q_vs_aperture_radius(
 
     # calculate the total count rates in the aperture, pretending all pixels were instead the median pixel value
     # innermost aperture is actually a circle
-    uw1_annular_count_rates = [uw1_annular_median_count_rates[0] * np.pi * drs[0] ** 2]
-    uvv_annular_count_rates = [uvv_annular_median_count_rates[0] * np.pi * drs[0] ** 2]
+    uw1_annular_count_rates = [
+        uw1_annular_analyses[0].median_count_rate * np.pi * drs[0] ** 2
+    ]
+    uvv_annular_count_rates = [
+        uvv_annular_median_count_rates[0].median_count_rate * np.pi * drs[0] ** 2
+    ]
     # and the rest are annuli
     uw1_annular_count_rates.extend(
-        [
-            2 * np.pi * x * dr
-            for x, dr in zip(uw1_annular_median_count_rates[1:], drs[1:])
-        ]
+        [2 * np.pi * x * dr for x, dr in zip(uw1_annular_analyses[1:], drs[1:])]
     )
     uvv_annular_count_rates.extend(
         [
@@ -297,7 +296,7 @@ def q_vs_aperture_radius_at_epoch(
         uvv_bg=uvv_bg,
         max_aperture_radius=200000 * u.km,  # type: ignore
         # max_aperture_radius=1000000 * u.km,  # type: ignore
-        num_apertures=200,
+        num_apertures=100,
     )
 
     by_dust_redness = lambda x: x.dust_redness
